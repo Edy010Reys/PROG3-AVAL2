6d1b990d283fc940bc9303a4332d2b654d8d289e
Edy010Reys <edson10vasconcelos@gmail.com>
2023-11-22 20:33:23 -0300

    Parte Client-Side

---
+++ client/js/candidates.js
@@ -0,0 +1,43 @@
+const input = document.querySelector('#input input')
+const cards = document.querySelector('#cards')
+
+input.addEventListener('input', async () => {
+  let search = {
+    name: input.value
+  }
+  let data = await postNameCadidante(search)
+
+  cards.innerHTML = ''
+  data.map((elem) => {
+    cards.insertAdjacentHTML('afterbegin',
+    `<ul>
+    <li id="card">
+      <h3>${elem.nome}</h3>
+      <p>Cargo: ${elem.cargo}</p>
+      <p>Quantidade de Votos: ${elem.votacao}</p>
+      <p>Status: ${elem.status}</p>
+    </li>
+    </ul>`);
+  })
+
+  if (input.value == '') {
+    cards.innerHTML = ''
+  }
+})
+
+async function postNameCadidante(search) {
+  return await fetch('http://localhost:3000/candidato', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(search)
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    return data;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
